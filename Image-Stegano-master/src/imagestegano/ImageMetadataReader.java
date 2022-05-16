4466b44dc83e1df6f326c519f03d262ad2ed701d
Emily Holleran <emilymarie2722@gmail.com>
2022-05-16 13:55:33 -0400

    Add files via upload

---
+++ Image-Stegano-master/src/imagestegano/ImageMetadataReader.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package imagestegano;
+
+import java.io.File;
+import jdk.jfr.internal.tool.Metadata;
+
+/**
+ *
+ * @author E_Holleran
+ */
+class ImageMetadataReader {
+
+    static Metadata readMetadata(File openedFile) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
